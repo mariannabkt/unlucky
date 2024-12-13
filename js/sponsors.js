78fee0bfdfddd4e9db52e4cd88aca6adfed43990
Marianna Bakti <mariannabkt@gmail.com>
2024-12-13 11:05:08 +0200

    added soponsors

--- js/sponsors.js
+++ js/sponsors.js
@@ -1,10 +1,20 @@
-function switchTab(tabId) {
-    // Hide all tab panes
-    const panes = document.querySelectorAll('.tab-pane');
-    panes.forEach(pane => (pane.style.display = 'none'));
-  
-    // Show the selected tab pane
-    const selectedPane = document.getElementById(tabId);
-    selectedPane.style.display = 'flex';
-  }
-  
+function switchTab(tab, tabId) {
+
+  const tabs = document.querySelectorAll('.tab');
+  tabs.forEach(tab => {
+    tab.classList.remove('selected');
+  });
+
+  const selectedTab = document.getElementById(tab);
+  selectedTab.classList.add('selected');
+
+  // Hide all tab panes
+  const panes = document.querySelectorAll('.tab-pane');
+  panes.forEach(pane => {
+    pane.style.display = 'none';
+  });
+
+  // Show the selected tab pane
+  const selectedPane = document.getElementById(tabId);
+  selectedPane.style.display = 'flex';
+}
